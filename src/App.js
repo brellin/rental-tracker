3a748daa4bf906f3d7f4dc9914e1c5f778dabe76
brellin <cekakid@gmail.com>
2022-08-31 01:49:11 -0500

    basic CRUD

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import { useEffect, useState } from 'react';
+
+import Row from './components/Row';
 import './App.scss';
 
-const serverAddress = window.location.hostname;
+const serverAddress = window.location.hostname === 'localhost' ? 'http://localhost:5000/rentals' : 'https://pfl-be.herokuapp.com/rentals';
 
-export default _ => {
+const App = _ => {
   const [ rentals, setRentals ] = useState([]);
+  const [ fields, setFields ] = useState({ name: '', link: '', contacted: false, scheduled: false, wanted: null });
 
   useEffect(_ => {
-    fetch(serverAddress === 'localhost' ? 'http://localhost:5000/rentals' : 'https://pfl-be.herokuapp.com/rentals')
+    fetch(serverAddress)
       .then(res => res.json())
       .then(res => setRentals(res))
       .catch(err => console.error(err));
   }, []);
 
-  console.log(rentals);
+  console.log(fields);
+
+  const handleChanges = e => { setFields({ ...fields, [ e.target.name ]: e.target.type === 'checkbox' ? e.target.checked : e.target.value }); };
+
+  const submit = e => {
+    e.preventDefault();
+    fetch(serverAddress + '/new', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(fields)
+    })
+      .then(res => {
+        console.log(res);
+        rentals.push(fields);
+        setFields({ name: '', link: '', contacted: false, scheduled: false, wanted: null });
+      })
+      .catch(err => console.error(err));
+  };
 
   return <div className="wrApp">
     <h1>Rental Tracker</h1>
@@ -21,16 +41,32 @@ export default _ => {
     <table>
       <thead>
         <tr>
+          <th>Name</th>
+          <th>Link</th>
           <th>Contacted</th>
           <th>Scheduled</th>
           <th>Wanted</th>
-          <th>Link</th>
-          <th>Name</th>
+          <th>Add/Delete</th>
         </tr>
       </thead>
 
       <tbody>
+        { rentals.map(r => <Row key={ r.id } { ...r } />) }
       </tbody>
+
+      <tfoot>
+        <tr>
+          <td> <input type="text" name='name' onInput={ handleChanges } /> </td>
+          <td> <input type="text" name='link' onInput={ handleChanges } /> </td>
+          <td> <input type="checkbox" name="contacted" onInput={ handleChanges } /> </td>
+          <td> <input type="checkbox" name="scheduled" onInput={ handleChanges } /> </td>
+          <td> <input type="checkbox" name="wanted" onInput={ handleChanges } /> </td>
+          <td colSpan={ 5 }><button onClick={ submit }>Submit</button></td>
+        </tr>
+      </tfoot>
     </table>
+
   </div>;
 };
+
+export default App;
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,41 @@
-export default ({ contacted, scheduled, wanted, link }) => {
+import { useState } from "react";
+
+export default ({ id, name, contacted, scheduled, wanted, link }) => {
+    const [ fields, setFields ] = useState({ name, link, contacted, scheduled, wanted });
+
+
+    const serverAddress = window.location.hostname === 'localhost' ? 'http://localhost:5000/rentals' : 'https://pfl-be.herokuapp.com/rentals';
+
+
+    const handleChanges = e => {
+        setFields({ ...fields, [ e.target.name ]: e.target.checked });
+
+        fetch(serverAddress + `/${ id }`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ [ e.target.name ]: e.target.checked })
+        })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => console.error(err));
+    };
+
+    const delRent = e => {
+        e.preventDefault();
+        fetch(serverAddress + `/${ id }`, { method: "DELETE" })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => console.error(err));
+    };
+
     return <tr>
-        <td>{ contacted }</td>
-        <td>{ scheduled }</td>
-        <td>{ wanted }</td>
-        <td>{ link }</td>
+        <td>{ name }</td>
+        <td><a about="_blank" href={ link }>{ link }</a></td>
+        <td><input type="checkbox" name="contacted" checked={ fields.contacted } onChange={ handleChanges } /></td>
+        <td><input type="checkbox" name="scheduled" checked={ fields.scheduled } onChange={ handleChanges } /></td>
+        <td><input type="checkbox" name="wanted" checked={ fields.wanted } onChange={ handleChanges } /></td>
+        <td><button className="del" onClick={ delRent }>X</button></td>
     </tr>;
 };
